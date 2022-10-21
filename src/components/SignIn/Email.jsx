2ea7dd863cf1736a92ad7a21209484df838e9c17
YANGDAYOUNG <jc1525@hanmail.net>
2022-10-21 20:57:23 +0900

    #227 feat: Email컴포넌트 공통으로 쓸 수 있게 바꿈

--- src/components/SignIn/Email.jsx
+++ src/components/SignIn/Email.jsx
@@ -1,21 +1,38 @@
 import React, { useState } from 'react'
-import NextBtn from '../common/NextBtn'
-import Modal from '../common/Modal'
 
-const Email = ({ onChange, alret }) => {
+//조사체크하는 함수!!
+function checkPostposition(name) {
+  //name의 마지막 음절의 유니코드(UTF-16)
+  const charCode = name.charCodeAt(name.length - 1)
+
+  //유니코드의 한글 범위 내에서 해당 코드의 받침 확인
+  const consonantCode = (charCode - 44032) % 28
+
+  if (consonantCode === 0) {
+    //0이면 받침 없음 -> 를
+    return `${name}를`
+  }
+  //1이상이면 받침 있음 -> 을
+  return `${name}을`
+}
+const Email = ({ onChange, alret, htmlFor, title }) => {
   return (
     <>
       <div>
-        <label htmlFor="email">이메일</label>
+        <label htmlFor={htmlFor}>{title}</label>
         <div className="mt-4 mb-3 relative flex box-border border border-neutral-200 rounded items-center border-box">
           <input
-            name="email"
-            placeholder="이메일을 입력 해 주세요."
+            name={htmlFor}
+            placeholder={`${checkPostposition(title)} 입력 해 주세요.`}
             className="px-3 h-[3rem] border-none flex-initial box-border w-full py-[0.75rem] rounded text-[0.875rem] transition shadow-white"
             onChange={onChange}
           />
         </div>
-        <p className="mt-[8px] font-[11px] text-red-600 text-[12px]">{alret}</p>
+        {alret && (
+          <p className="mt-[8px] font-[11px] text-red-600 text-[12px]">
+            {alret}
+          </p>
+        )}
       </div>
     </>
   )
