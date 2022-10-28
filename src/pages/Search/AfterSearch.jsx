729ddf6b65562096dfd013608eb94db49d7ffbde
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-10-28 23:08:35 +0900

    feat: 검색 기능 프론트단에서 구현

--- src/pages/Search/AfterSearch.jsx
+++ src/pages/Search/AfterSearch.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import HaveResult from './HaveResult'
 import NoResult from './NoResult'
 import product from '~/dummy/dummy/product.json'
+import { useGetProductsQuery } from '../../store/api/productApiSlice'
+import { useParams } from 'react-router-dom'
 
 function AfterSearch() {
+  const { search } = useParams()
+  const { data } = useGetProductsQuery()
+  const product2 = data?.filter((item) => item.productName.includes(search))
+  const product3 = data?.filter((item) => item.brandKo.includes(search))
+  const products = product2.concat(product3)
+
   return (
     <div className="mt-7">
-      {product && product.length !== 0 ? (
-        <HaveResult product={product} />
+      {products && products.length !== 0 ? (
+        <HaveResult product={products} />
       ) : (
         <NoResult />
       )}
